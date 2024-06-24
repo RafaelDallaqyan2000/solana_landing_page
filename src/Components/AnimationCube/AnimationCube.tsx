94a08a7ced3dd94ca5a324576e1e1109f08a695d
Rafael Dallaqyan <94279762+RafaelDallaqyan2000@users.noreply.github.com>
2024-06-24 15:08:38 +0400

    animation cahnges

---
+++ src/Components/AnimationCube/AnimationCube.tsx
@@ -0,0 +1,10 @@
+import "./animationCubeStyle.css";
+
+export const AnimationCube = () => {
+  return (
+    <div className="animatioContainer">
+      <div className="square1" />
+      <div className="square2" />
+    </div>
+  );
+};
--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -3,24 +3,28 @@ import { useTranslation } from "react-i18next";
 import { Box, Typography } from "@mui/material";
 import { LanguageSwitcher } from "../LanguageSwitcher";
 import logo from "../../images/solana.png";
+import { AnimationCube } from "../AnimationCube/AnimationCube";
 
 export const Header: React.FC = () => {
   const { t } = useTranslation();
 
   return (
-    <Box textAlign="center" my={4}>
-      <Box sx={{ textAlign: "end" }}>
-        <LanguageSwitcher />
+    <>
+      <AnimationCube />
+      <Box textAlign="center" my={4}>
+        <Box sx={{ textAlign: "end" }}>
+          <LanguageSwitcher />
+        </Box>
+        <Box mb={2}>
+          <img src={logo} alt="Company Logo" style={{ maxWidth: "250px" }} />
+        </Box>
+        <Typography variant="h2" component="h1" gutterBottom>
+          {t("welcome")}
+        </Typography>
+        <Typography variant="h5" component="p" gutterBottom>
+          {t("description")}
+        </Typography>
       </Box>
-      <Box mb={2}>
-        <img src={logo} alt="Company Logo" style={{ maxWidth: "250px" }} />
-      </Box>
-      <Typography variant="h2" component="h1" gutterBottom>
-        {t("welcome")}
-      </Typography>
-      <Typography variant="h5" component="p" gutterBottom>
-        {t("description")}
-      </Typography>
-    </Box>
+    </>
   );
 };
