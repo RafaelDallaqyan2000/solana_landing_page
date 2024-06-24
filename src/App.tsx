a9261f580da4601bc09b4ceed80f97383a51f9a3
Rafael Dallaqyan <94279762+RafaelDallaqyan2000@users.noreply.github.com>
2024-06-24 12:56:57 +0400

    code refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,13 @@
 import "./App.css";
 import { Home } from "./pages";
-import { Provider } from "react-redux";
-import store from "./store";
 import { ThemeProvider } from "@mui/material";
 import theme from "./providers";
 
 function App() {
   return (
-    <Provider store={store}>
-      <ThemeProvider theme={theme}>
-        <Home />
-      </ThemeProvider>
-    </Provider>
+    <ThemeProvider theme={theme}>
+      <Home />
+    </ThemeProvider>
   );
 }
 
--- src/store/index.ts
+++
@@ -1,3 +0,0 @@
-export {default} from './configureStore';
-
-export {handleChange} from './actions';
